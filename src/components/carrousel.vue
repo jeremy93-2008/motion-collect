d4b281255ac932334ae638ab17b7e0579da0f827
Jeremy Alvarez <jeremy932012@gmail.com>
2024-03-01 01:49:56 +0100

    featL: add carrousel inde page

---
+++ src/components/carrousel.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+import type { IMovieDB } from '~/types/MovieDB.type'
+
+defineProps({
+    data: Array,
+})
+</script>
+
+<template>
+    <section class="carrousel_container">
+        <ul class="carrousel">
+            <li v-for="item in data" :key="item.id">
+                <img
+                    :src="
+                        'https://media.themoviedb.org/t/p/w1920_and_h800_multi_faces' +
+                        item.backdrop_path
+                    "
+                    alt="backdrop"
+                />
+                <!--<img
+              :src="
+                  'https://image.tmdb.org/t/p/w300_and_h450_bestv2' +
+                  item.poster_path
+              "
+              alt="poster"
+          />-->
+            </li>
+        </ul>
+    </section>
+</template>
+
+<style scoped>
+ul.carrousel {
+    display: flex;
+    overflow-x: auto;
+    scroll-snap-type: x mandatory;
+    scroll-behavior: smooth;
+    padding: 1rem;
+    list-style: none;
+    margin: 0;
+}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
+import type { IMovieDB } from '~/types/MovieDB.type'
+import Carrousel from '~/components/carrousel.vue'
 
+const { data } = await useFetch<IMovieDB>(
+    'https://api.themoviedb.org/3/trending/all/day?language=en-US',
+    {
+        headers: {
+            Authorization:
+                'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYTc3YjlmMjNmMzk2ZTZkODUzZTMyYzJhYzQwMjE4OSIsInN1YiI6IjYyYTRiZmQ2N2Y2YzhkMzYwZDdjOWVkYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4uuA3irBz-yTHky4z5zdvEx8WXuT7EdsgRGHtBnTRh8',
+        },
+    }
+)
 </script>
 
 <template>
-  Hola mundo
+    <Carrousel :data="(data as IMovieDB).results" />
 </template>
